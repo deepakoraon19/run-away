d24125d7f6fa2eb4d87688c5d7371d0035e5710c
Deepak Oraon <deepak.oraon@alumnux.com>
2024-11-24 21:06:17 +0530

    session storage fix

--- app/chat-room/page.tsx
+++ app/chat-room/page.tsx
@@ -2,15 +2,18 @@
 import MessageBox from '@/components/MessageBox'
 import { connection } from '@/services/wsService'
 import { Stack, Typography, TextField, Button } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import SendIcon from '@mui/icons-material/Send';
 function ChatRoom() {
     const [message, setMessage] = React.useState("")
-    const group = sessionStorage.getItem("groupName")
-    const user = sessionStorage.getItem("userName")
-
+    let group = ""
+    let user = ""
+    useEffect(() => {
+        group = sessionStorage.getItem("groupName") ?? ""
+        user = sessionStorage.getItem("userName") ?? ""
+    },[])
     const onSend = () => {
-        connection.invoke("SendMessageToRoom", group, user,message)
+        connection.invoke("SendMessageToRoom", group, user, message)
     }
     return (
         <Stack width={"100vw"} height={"100vh"} alignItems={"center"} gap={6}>
@@ -19,7 +22,7 @@ function ChatRoom() {
                 <MessageBox />
                 <Stack direction={"row"} gap={2} justifyContent={"space-around"}>
                     <TextField onChange={p => setMessage(p.target.value)} value={message} id="standard-basic" placeholder='Message' variant="standard" />
-                    <Button variant="contained" startIcon={<SendIcon/>} onClick={onSend}></Button>
+                    <Button variant="contained" startIcon={<SendIcon />} onClick={onSend}></Button>
                 </Stack>
             </Stack>
         </Stack>
